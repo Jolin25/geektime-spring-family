bb5391bf8f31e2f9b0f70a854873d8c2f7cc1462
jrl <584189010@qq.com>
2022-03-15 12:48:10 +0800

    stream : flatMap

--- FAQ 2019-03/stream-demo/src/main/java/geektime/spring/faq/Demo.java
+++ FAQ 2019-03/stream-demo/src/main/java/geektime/spring/faq/Demo.java
@@ -1,5 +1,6 @@
 package geektime.spring.faq;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,10 +15,19 @@ public class Demo {
                 .map(s -> Arrays.asList(s))
                 .flatMap(l -> l.stream())
                 .forEach(System.out::println);
-
-        Arrays.asList("Foo", "Bar").stream()
-                .map(s -> Arrays.asList(s))
+        List<List<String>> a = new ArrayList<>();
+        List<String> a1 = new ArrayList<>();
+        a1.add("a11");
+        a1.add("a12");
+        List<String> a2 = new ArrayList<>();
+        a2.add("a21");
+        a2.add("a22");
+        a.add(a1);
+        a.add(a2);
+        a.stream()
                 .flatMap(l -> l.stream())
+                .forEach(System.out::println);
+        a.stream()
                 .forEach(System.out::println);
         /** knowledge point:  也就是说stream的操作没有改变原数据*/
         String[] stArr = {"aa", "bb"};
