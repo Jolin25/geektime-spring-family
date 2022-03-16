5baa9aee82269fed6145262e589c9f57ae0fec6e
jrl <584189010@qq.com>
2022-03-16 17:58:31 +0800

    Spring：RestfulTemplate

--- Chapter 7/simple-resttemplate-demo/src/main/java/geektime/spring/springbucks/customer/CustomerServiceApplication.java
+++ Chapter 7/simple-resttemplate-demo/src/main/java/geektime/spring/springbucks/customer/CustomerServiceApplication.java
@@ -21,41 +21,47 @@ import java.net.URI;
 @SpringBootApplication
 @Slf4j
 public class CustomerServiceApplication implements ApplicationRunner {
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder()
-				.sources(CustomerServiceApplication.class)
-				.bannerMode(Banner.Mode.OFF)
-				.web(WebApplicationType.NONE)
-				.run(args);
-	}
+    public static void main(String[] args) {
+        /** knowledge point:
+         *  因为正常来说Spring Boot 会启动一个tomcat，把这个项目运行在8080端口上（因为导入了web依赖）
+         *  但是这个项目不需要，所以做了相关配置
+         * */
+        new SpringApplicationBuilder()
+                .sources(CustomerServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                // 表示不需要启动tomcat
+                // TODO_Joly:为什么不需要，所以这RestTemplate去访问的是谁
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
 //		return new RestTemplate();
-		return builder.build();
-	}
+        return builder.build();
+    }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		URI uri = UriComponentsBuilder
-				.fromUriString("http://localhost:8080/coffee/{id}")
-				.build(1);
-		ResponseEntity<Coffee> c = restTemplate.getForEntity(uri, Coffee.class);
-		log.info("Response Status: {}, Response Headers: {}", c.getStatusCode(), c.getHeaders().toString());
-		log.info("Coffee: {}", c.getBody());
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:8080/coffee/{id}")
+                .build(1);
+        ResponseEntity<Coffee> c = restTemplate.getForEntity(uri, Coffee.class);
+        log.info("Response Status: {}, Response Headers: {}", c.getStatusCode(), c.getHeaders().toString());
+        log.info("Coffee: {}", c.getBody());
 
-		String coffeeUri = "http://localhost:8080/coffee/";
-		Coffee request = Coffee.builder()
-				.name("Americano")
-				.price(BigDecimal.valueOf(25.00))
-				.build();
-		Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
-		log.info("New Coffee: {}", response);
+        String coffeeUri = "http://localhost:8080/coffee/";
+        Coffee request = Coffee.builder()
+                .name("Americano")
+                .price(BigDecimal.valueOf(25.00))
+                .build();
+        Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
+        log.info("New Coffee: {}", response);
 
-		String s = restTemplate.getForObject(coffeeUri, String.class);
-		log.info("String: {}", s);
-	}
+        String s = restTemplate.getForObject(coffeeUri, String.class);
+        log.info("String: {}", s);
+    }
 }
