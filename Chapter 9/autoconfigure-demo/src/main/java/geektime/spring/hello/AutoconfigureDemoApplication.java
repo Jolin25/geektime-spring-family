03c6d87da2f7816cb40cdae5d837700e64f50dbe
JolyPhone <18990341730@163.com>
2022-03-21 20:17:28 +0800

    Spring Boot：实现自动化配置

--- Chapter 9/autoconfigure-demo/src/main/java/geektime/spring/hello/AutoconfigureDemoApplication.java
+++ Chapter 9/autoconfigure-demo/src/main/java/geektime/spring/hello/AutoconfigureDemoApplication.java
@@ -2,7 +2,7 @@ package geektime.spring.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Spring Boot 会去扫描这个注解所在包之下的所有类的配置，所以一般就是整个项目了
 @SpringBootApplication
 public class AutoconfigureDemoApplication {
 
--- Chapter 9/geektime-autoconfigure-backport/src/main/java/geektime/spring/hello/greeting/GreetingAutoConfiguration.java
+++ Chapter 9/geektime-autoconfigure-backport/src/main/java/geektime/spring/hello/greeting/GreetingAutoConfiguration.java
@@ -2,7 +2,12 @@ package geektime.spring.hello.greeting;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+/**
+ * 这个作为自动配置类
+ * 这个自动配置类的作用是将Greeting实例放入容器中
+* @author jrl
+* @date 2022/3/21
+*/
 @Configuration
 public class GreetingAutoConfiguration {
     @Bean
