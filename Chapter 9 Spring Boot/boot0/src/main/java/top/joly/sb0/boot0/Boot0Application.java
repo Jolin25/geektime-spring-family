bef81594cea7d17f37b293d3c0e9edb97911180f
Joly163 <18990341730@163.com>
2022-12-05 19:00:26 +0800

    创建 Spring Boot 模块，用于学习 Spring Boot

---
+++ Chapter 9 Spring Boot/boot0/src/main/java/top/joly/sb0/boot0/Boot0Application.java
@@ -0,0 +1,16 @@
+package top.joly.sb0.boot0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joly
+ */
+@SpringBootApplication
+public class Boot0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot0Application.class, args);
+    }
+
+}
---
+++ Chapter 9 Spring Boot/boot0/src/test/java/top/joly/sb0/boot0/Boot0ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.joly.sb0.boot0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
